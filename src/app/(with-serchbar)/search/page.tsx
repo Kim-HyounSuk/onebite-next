8145271db2c47c3c034dd5e8ccf7d6688bb76450
Kim-HyounSuk <kkoor586@naver.com>
2024-11-18 04:39:24 +0900

    feat: Search 페이지 작성

--- src/app/(with-serchbar)/search/page.tsx
+++ src/app/(with-serchbar)/search/page.tsx
@@ -1,13 +1,15 @@
-import books from '@/mock/books.json';
 import BookItem from '@/components/BookItem';
+import { IBookData } from '@/types/book';
 
-export default function Page({
-	searchParams,
-}: {
-	searchParams: {
-		q?: string;
-	};
-}) {
+export default async function Page({ searchParams }: { searchParams: { q?: string } }) {
+	const { q } = await searchParams;
+	const res = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`);
+	if (!res.ok) {
+		return <div>오류가 발생했습니다.</div>;
+	}
+
+	const books: IBookData[] = await res.json();
+
 	return (
 		<div>
 			{books.map((book) => (
